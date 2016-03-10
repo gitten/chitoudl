bc51fb6d5d326ffe3c3423f05fa17a0fe67a03e2
gitten <gitten@gitten.com>
2016-03-09 15:46:58 -1000

    2nd iteration logic working for StartApp.Simple

--- web/elm/Chit.elm
+++ web/elm/Chit.elm
@@ -1,178 +1,175 @@
 module Chit where
 
+import String exposing (join)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Signal exposing (Address, Mailbox, mailbox)
 
-import StartApp
+import StartApp.Simple as StartApp
 import Effects exposing (Effects, Never)
 import Task exposing (Task)
 
-main : Signal Html
-main =
-  app.html
 
 
-app =
-  StartApp.start
-            { init = model
-            , update = update
-            , view = view
-            , inputs = [incomingChit]
-            }
-
-
-port tasks : Signal (Task Never ())
-port tasks =
-  app.tasks
-
-
-port inchits : Signal Chit
-
-               
-incomingChit =
-  Signal.map ReceiveChit inchits
- 
-
----- model
-
+---- MODEL
 
 type alias Model =
   { userName : String
-  , chitToSend : String
-  , chits : List Chit
+  , chitRooms : List String
+  , chitData : List Chit
+  , currentRoom : String
+  , chit2send : String
   }
 
-                 
+
 type alias Chit =
-  { user : String
-  , body : String
+  { roomName : String
+  , user : String
+  , msg : String
   }
 
 
-model : (Model, Effects Action)
-model =
-  let
-    model =
-      { userName = "useMePls"
-      , chitToSend = ""
-      , chits = [initChit]
-      }
-  in
-    (model, Effects.none)
-
-
-initChit =
-  { user = "your face hyar"
-  , body = "your poops hyar"
+init : Model
+init =
+  { userName = "yurnaim"
+  , chitRooms = initRooms
+  , chitData = genChit :: moreChit ++ initChitData
+  , currentRoom = "general"
+  , chit2send = ""
   }
 
 
----- update
-
-type Action
-  = UpdateUserName String
-  | Chitting String
-  | SendChit
-  | ChitSent
-  | ReceiveChit Chit
+initRooms : List String
+initRooms = [ "general", "ninja", "pirate", "unicorn", "rainbow" ]
 
 
-chitFormat : String -> String -> Chit    
-chitFormat userName chitToSend =
-  { user = userName, body = chitToSend }
+initChitData =
+    List.map initChit initRooms
 
 
-update : Action -> Model -> (Model, Effects Action)
-update action model =
-  case action of
-    UpdateUserName str ->
-      ({ model | userName = str }, Effects.none)
+initChit : String -> Chit
+initChit string =
+  { roomName = string
+  , user = "chitBot"
+  , msg = " your base R belonging to I"
+  }
 
-    Chitting str ->
-      ({ model | chitToSend = str }, Effects.none)
-
-    ReceiveChit chit ->
-      ({ model | chits = chit :: model.chits }, Effects.none)
-                    
-    SendChit ->
-      (model, sendChitFx model.userName model.chitToSend)
-
-    ChitSent ->
-      ({model | chitToSend = ""}, Effects.none)
-
-----Output chits to js
-port outgoingChit : Signal Chit
-port outgoingChit =
-  portOutgoingChitMb.signal
-                
-portOutgoingChitMb : Mailbox Chit
-portOutgoingChitMb =
-  mailbox initChit
-
-format2chit : String -> String -> Chit
-format2chit userName msg =
-  { user = userName, body = msg }
-
-sendChitFx : String -> String -> Effects Action    
-sendChitFx user msg =
-  format2chit user msg
-    |> Signal.send portOutgoingChitMb.address
-    |> Effects.task
-    |> Effects.map (\_ -> ChitSent)
+--------test data
+genChit =
+  {roomName = "general", user = "Kyle", msg = " I R lisp"}
 
 
-
-
-
-
-
-
----- view
-
-view : Address Action -> Model -> Html
-view action model =
-  div
-    [ id "chitoudl" ]
-    [ chitWindow action model
-    , chiterer action model 
-    ]
-
-
-chitWindow : Address Action -> Model -> Html
-chitWindow _ model =
-  let
-    newChit chit =
-      p
-        []
-        [ text ("[" ++ chit.user ++ "] : " ++ chit.body) ]
-  in
-    div
-      [ id "messages" ]
-      (List.reverse (List.map newChit  model.chits))
-
+moreChit =
+  [ {roomName = "ninja", user = "a wild ninja", msg = " I R Ninjer"}
+  , {roomName = "pirate", user = "a wild pirate", msg = " I Arrrrrg"}
+  , {roomName = "unicorn", user = "a wild unicorn", msg = " I R Neeey?"}
+  , {roomName = "rainbow", user = "skittles", msg = " I R taste"}
+  ]
     
 
-chiterer : Address Action -> Model -> Html
-chiterer address model =
+----UPDATE
+type Action
+  = ChangeRoom String
+  | Chitting String
+  | SendChit
+  | UpdateName String
+  | NoOp
+
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Chitting message ->
+      { model | chit2send = message }
+                                
+    ChangeRoom room ->
+      { model | currentRoom = room }
+
+    SendChit ->
+      { model | chit2send = "" }
+
+    UpdateName name ->
+      { model | userName = name }
+
+    NoOp -> model
+
+----VIEW
+
+view : Address Action -> Model -> Html
+view address model =
   div
-    [id "footer"]
+    [ id "chitoudl" ]
+    [ viewRooms address model
+    , viewChits model
+    , viewChiterer address model
+    ]
+
+viewRooms : Address Action -> Model -> Html
+viewRooms address model =
+  let
+    clickEvent room =
+      onClick address (ChangeRoom room)
+  in
+    div
+      [ id "rooms" ]
+      (List.map (\s -> li [ clickEvent s ] [text s]) model.chitRooms)
+
+
+viewChits : Model -> Html
+viewChits model =
+  div
+    [ id "chits" ]
+    ( model.chitData
+      |> List.filter (isRoom model.currentRoom)
+      |> List.map formatChit
+      |> List.reverse
+    )
+
+isRoom : String -> Chit -> Bool
+isRoom currentRoom {roomName} =
+  if roomName == currentRoom then True else False
+        
+
+viewChiterer : Address Action -> Model -> Html      
+viewChiterer address model =
+  div
+    [ id "footer" ]
     [ input
         [ value model.userName
-        , on "input" targetValue (Signal.message address << UpdateUserName)
+        , on "input" targetValue (Signal.message address << UpdateName)
         ]
         []
 
-    , text " : "
-           
     , input
-        [ placeholder "my body"
-        , value model.chitToSend
+        [ placeholder "the body here"
+        , value model.chit2send
         , on "input" targetValue (Signal.message address << Chitting)
         ]
         []
-        
-    , button [ onClick address SendChit ] [ text "send" ]
+
+    , button [ onClick address SendChit ] [ text "Send" ]
     ]
 
+
+formatChit : Chit -> Html
+formatChit { user, msg} =
+  p
+    []
+    [ String.join "" [ "[ ", user, " ] : ", msg ] |> text ]
+
+
+
+
+  
+
+
+----WIRING
+
+main =
+  StartApp.start
+            { view = view
+            , update = update
+            , model = init
+            }
