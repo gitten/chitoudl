fcaa24177e14b5ee620c1356f487d6063b728fea
gitten <gitten@gitten.com>
2016-03-12 20:18:56 -1000

    still dont know how to git

--- web/elm/Chit.elm
+++ web/elm/Chit.elm
@@ -7,19 +7,15 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Signal exposing (Address, Mailbox, mailbox)
 
-import StartApp.Simple as StartApp
+import StartApp
 import Effects exposing (Effects, Never)
 import Task exposing (Task)
 
-<<<<<<< HEAD
 import Debug
 
 
 
 
-=======
-
->>>>>>> bc51fb6d5d326ffe3c3423f05fa17a0fe67a03e2
 
 ---- MODEL
 
@@ -33,79 +29,29 @@ type alias Model =
 
 
 type alias Chit =
-<<<<<<< HEAD
   { roomname : String
-=======
-  { roomName : String
->>>>>>> bc51fb6d5d326ffe3c3423f05fa17a0fe67a03e2
   , user : String
   , msg : String
   }
     
 
 
-<<<<<<< HEAD
 
 ----UPDATE
-=======
-init : Model
-init =
-  { userName = "yurnaim"
-  , chitRooms = initRooms
-  , chitData = genChit :: moreChit ++ initChitData
-  , currentRoom = "general"
-  , chit2send = ""
-  }
 
-
-initRooms : List String
-initRooms = [ "general", "ninja", "pirate", "unicorn", "rainbow" ]
-
-
-initChitData =
-    List.map initChit initRooms
-
-
-initChit : String -> Chit
-initChit string =
-  { roomName = string
-  , user = "chitBot"
-  , msg = " your base R belonging to I"
-  }
-
---------test data
-genChit =
-  {roomName = "general", user = "Kyle", msg = " I R lisp"}
-
->>>>>>> bc51fb6d5d326ffe3c3423f05fa17a0fe67a03e2
-
-moreChit =
-  [ {roomName = "ninja", user = "a wild ninja", msg = " I R Ninjer"}
-  , {roomName = "pirate", user = "a wild pirate", msg = " I Arrrrrg"}
-  , {roomName = "unicorn", user = "a wild unicorn", msg = " I R Neeey?"}
-  , {roomName = "rainbow", user = "skittles", msg = " I R taste"}
-  ]
-    
-
-----UPDATE
 type Action
   = ChangeRoom String
   | Chitting String
   | SendChit
   | UpdateName String
-<<<<<<< HEAD
   | ReceiveChit Chit
   | ChitSent
-=======
-  | NoOp
->>>>>>> bc51fb6d5d326ffe3c3423f05fa17a0fe67a03e2
 
 
-update : Action -> Model -> Model
+update : Action -> Model -> (Model, Effects Action)
 update action model =
   case action of
     Chitting message ->
-<<<<<<< HEAD
       ({ model | chit2send = message }, Effects.none)
                                 
     ChangeRoom room ->
@@ -142,35 +88,15 @@ format2chit room userName msg =
 
 
 
-=======
-      { model | chit2send = message }
-                                
-    ChangeRoom room ->
-      { model | currentRoom = room }
-
-    SendChit ->
-      { model | chit2send = "" }
-
-    UpdateName name ->
-      { model | userName = name }
-
-    NoOp -> model
-
->>>>>>> bc51fb6d5d326ffe3c3423f05fa17a0fe67a03e2
 ----VIEW
 
 view : Address Action -> Model -> Html
 view address model =
   div
     [ id "chitoudl" ]
-<<<<<<< HEAD
     [ div
         [ id "chit-container" ]
         [ viewRooms address model, viewChits model ]
-=======
-    [ viewRooms address model
-    , viewChits model
->>>>>>> bc51fb6d5d326ffe3c3423f05fa17a0fe67a03e2
     , viewChiterer address model
     ]
 
@@ -180,7 +106,6 @@ viewRooms address model =
     clickEvent room =
       onClick address (ChangeRoom room)
   in
-<<<<<<< HEAD
     ul
       [ id "rooms" ]
       ( List.map
@@ -199,6 +124,7 @@ viewChits model =
     |> List.reverse
     )
     
+
 isRoom : String -> Chit -> Bool
 isRoom currentRoom {roomname} =
   if roomname == currentRoom then True else False
@@ -211,45 +137,13 @@ viewChiterer address model =
     [ input
         [ id "input-name"
         , value model.userName
-=======
-    div
-      [ id "rooms" ]
-      (List.map (\s -> li [ clickEvent s ] [text s]) model.chitRooms)
-
-
-viewChits : Model -> Html
-viewChits model =
-  div
-    [ id "chits" ]
-    ( model.chitData
-      |> List.filter (isRoom model.currentRoom)
-      |> List.map formatChit
-      |> List.reverse
-    )
-
-isRoom : String -> Chit -> Bool
-isRoom currentRoom {roomName} =
-  if roomName == currentRoom then True else False
-        
-
-viewChiterer : Address Action -> Model -> Html      
-viewChiterer address model =
-  div
-    [ id "footer" ]
-    [ input
-        [ value model.userName
->>>>>>> bc51fb6d5d326ffe3c3423f05fa17a0fe67a03e2
         , on "input" targetValue (Signal.message address << UpdateName)
         ]
         []
 
     , input
-<<<<<<< HEAD
         [ id "input-msg"
         , placeholder "the body here"
-=======
-        [ placeholder "the body here"
->>>>>>> bc51fb6d5d326ffe3c3423f05fa17a0fe67a03e2
         , value model.chit2send
         , on "input" targetValue (Signal.message address << Chitting)
         ]
@@ -268,7 +162,6 @@ formatChit { user, msg} =
 
 
 
-<<<<<<< HEAD
 ----WIRING
 
 main = app.html
@@ -350,16 +243,3 @@ moreChit =
   , {roomname = "unicorn", user = "a wild unicorn", msg = " I R Neeey?"}
   , {roomname = "rainbow", user = "skittles", msg = " I R taste"}
   ]
-=======
-  
-
-
-----WIRING
-
-main =
-  StartApp.start
-            { view = view
-            , update = update
-            , model = init
-            }
->>>>>>> bc51fb6d5d326ffe3c3423f05fa17a0fe67a03e2
